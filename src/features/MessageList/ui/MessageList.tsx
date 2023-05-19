de60b056b1e29bd1c2e1e3355e9ae11b56699cdf
Вячеслав <90838956+arustomyan@users.noreply.github.com>
2023-05-19 17:44:01 +0300

    feat: added chat scrolling to the beginning

--- src/features/MessageList/ui/MessageList.tsx
+++ src/features/MessageList/ui/MessageList.tsx
@@ -1,7 +1,7 @@
 import { Message } from "entities/Message";
 import style from "./MessageList.module.css";
 
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import { Button } from "shared/ui/Button/Button";
 
 interface MessageListProps {
@@ -13,8 +13,18 @@ export const MessageList: FC<MessageListProps> = ({
   messageList,
   handleGetChatList,
 }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  console.log(ref);
+
+  useEffect(() => {
+    if (ref !== null) {
+      console.log("kmkmkm");
+      ref.current?.scrollBy(0, 999999);
+    }
+  }, [messageList]);
   return (
-    <div className={style.chat}>
+    <div className={style.chat} ref={ref}>
       {messageList
         .reverse()
         .map((message) => (
